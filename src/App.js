22b95cef9bb435d1a03bc853573352a0031f8288
bhushanhr26 <58306435+bhushanhr26@users.noreply.github.com>
2024-02-09 23:45:58 +0530

    feat: CI added for testing

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ function App() {
   return (
     <div className="App">
       <LoginPage />
-      {/* <h1>Hello world</h1>
-      <iframe src="https://www.w3schools.com/html/html_iframe.asp" aria-description="LKFNlskfnfd"></iframe> */}
+      <h1>Hello world</h1>
+      <iframe
+        src="https://www.w3schools.com/html/html_iframe.asp"
+        aria-description="LKFNlskfnfd"
+      ></iframe>
     </div>
   );
 }
